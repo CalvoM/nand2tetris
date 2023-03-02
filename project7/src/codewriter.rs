50cbbb372b1770e81980b58e1fd91e46aa942289
CalvoM <mwadimemakokha@gmail.com>
2023-03-02 23:41:11 +0300

    project7: arithmetic done, push started

--- project7/src/codewriter.rs
+++ project7/src/codewriter.rs
@@ -3,9 +3,13 @@ use std::{collections::HashMap, fs::File, io::Write};
 
 const X_VAR: &str = "@R14";
 const Y_VAR: &str = "@R15";
+const TEMP_VAR: &str = "@R13";
 pub struct CodeWriter {
     file: File,
     segments: HashMap<String, String>,
+    line_number: usize,
+    comp_line: String,
+    end_comp_line: String,
 }
 impl CodeWriter {
     pub fn new(output_file: &str) -> Self {
@@ -17,6 +21,9 @@ impl CodeWriter {
         Self {
             file: File::create(output_file).unwrap(),
             segments: map,
+            line_number: 0,
+            comp_line: String::new(),
+            end_comp_line: String::new(),
         }
     }
     pub fn write(&mut self, parser_results: Vec<ParserResult>) {
@@ -40,9 +47,9 @@ impl CodeWriter {
         self.decr_sp();
         match command.as_str() {
             "add" | "sub" | "eq" | "gt" | "lt" | "and" | "or" => {
-                self.set_x();
-                self.decr_sp();
                 self.set_y();
+                self.decr_sp();
+                self.set_x();
             }
             "neg" | "not" => {
                 self.set_y();
@@ -50,13 +57,20 @@ impl CodeWriter {
             _ => todo!(),
         }
         match command.as_str() {
-            "add" | "sub" => {
+            "add" | "sub" | "and" | "or" => {
                 self.get_x();
-                self.add_or_sub(&command);
+                self.add_sub_and_or(&command);
+                self.incr_sp();
             }
             "neg" | "not" => {
                 self.get_y();
-                self.neg_or_not(&command);
+                self.neg_not(&command);
+                self.incr_sp();
+            }
+            "gt" | "lt" | "eq" => {
+                self.write_default_set_false();
+                self.get_x();
+                self.logical_cmd(&command);
                 self.incr_sp();
             }
             _ => todo!(),
@@ -73,10 +87,45 @@ impl CodeWriter {
         let cmd = format!("// {:?} {} {}\r\n", command, segment, index);
         self.file.write(cmd.as_bytes()).unwrap();
         match command {
-            CommandType::C_POP => self.decr_sp(),
-            CommandType::C_PUSH => self.incr_sp(),
+            CommandType::C_POP => {
+                self.decr_sp();
+            }
+            CommandType::C_PUSH => {
+                self.handle_push_segment(segment.as_str(), index);
+                self.incr_sp()
+            }
             _ => todo!(),
         }
+    }
+    fn handle_push_segment(&mut self, segment: &str, index: u64) {
+        match segment {
+            "constant" => {
+                self.push_constant(index);
+            }
+            _ => todo!(),
+        }
+    }
+    fn push_constant(&mut self, index: u64) {
+        self.write_cmd(&mut [
+            "//Push Constant",
+            format!("@{}", index).as_str(),
+            "D=A",
+            "@SP",
+            "A=M",
+            "M=D",
+        ]);
+    }
+    fn write_set_true(&mut self) {
+        self.write_cmd(&mut [
+            "//Set true",
+            format!("({})", self.comp_line.clone().as_str()).as_str(),
+            TEMP_VAR,
+            "M=-1",
+            format!("({})", self.end_comp_line).as_str(),
+        ])
+    }
+    fn write_default_set_false(&mut self) {
+        self.write_cmd(&mut ["//Set false", TEMP_VAR, "M=0"])
     }
     fn incr_sp(&mut self) {
         self.write_cmd(&mut ["// Incr SP", "@SP", "M=M+1"]);
@@ -96,23 +145,48 @@ impl CodeWriter {
     fn get_y(&mut self) {
         self.write_cmd(&mut ["//Get y", Y_VAR, "D=M"]);
     }
-    fn add_or_sub(&mut self, op: &str) {
-        let mut sign = "+";
-        if op == "sub" {
-            sign = "-";
-        }
+    fn add_sub_and_or(&mut self, op: &str) {
+        let sign = match op {
+            "add" => "+",
+            "sub" => "-",
+            "and" => "&",
+            "or" => "|",
+            _ => "+",
+        };
         self.write_cmd(&mut [Y_VAR, format!("D=D{}M", sign).as_str(), "@SP", "A=M", "M=D"]);
     }
-    fn neg_or_not(&mut self, op: &str) {
+    fn neg_not(&mut self, op: &str) {
         let mut sign = "-";
         if op == "not" {
             sign = "!";
         }
         self.write_cmd(&mut [format!("D={}D", sign).as_str(), "@SP", "A=M", "M=D"]);
     }
+    fn logical_cmd(&mut self, op: &str) {
+        let jmp = match op {
+            "eq" => "JEQ",
+            "gt" => "JGT",
+            "lt" => "JLT",
+            _ => todo!(),
+        };
+        self.comp_line = format!("SET_TRUE_{}", self.line_number);
+        self.end_comp_line = format!("END_TRUE_{}", self.line_number);
+        self.write_cmd(&mut [
+            Y_VAR,
+            "D=D-M",
+            format!("@{}", self.comp_line).as_str(),
+            format!("D;{}", jmp).as_str(),
+            format!("@{}", self.end_comp_line).as_str(),
+            "0;JMP",
+        ]);
+        self.write_set_true();
+        //TODO: we will have to find how to push better
+        self.write_cmd(&mut [TEMP_VAR, "D=M", "@SP", "A=M", "M=D"]);
+    }
     fn write_cmd(&mut self, cmd_args: &mut [&str]) {
         let mut cmd = cmd_args.join("\r\n");
         cmd.push_str("\r\n");
+        self.line_number += cmd_args.len();
         self.file.write(cmd.as_bytes()).unwrap();
     }
     fn close(&mut self) {}
