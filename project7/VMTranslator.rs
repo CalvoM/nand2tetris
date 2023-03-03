a180f67c7005765d2fa9521e2d6f39524e9a2341
CalvoM <mwadimemakokha@gmail.com>
2023-03-03 20:20:29 +0300

    project7: done and submitted

---
+++ project7/VMTranslator.rs
@@ -0,0 +1,20 @@
+use std::env;
+pub mod codewriter;
+pub mod commandtype;
+pub mod parser;
+pub use codewriter::CodeWriter;
+pub use commandtype::CommandType;
+pub use parser::Parser;
+pub use parser::ParserResult;
+
+fn main() {
+    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let input_file_name = &args[1];
+    let root_file_name = input_file_name.split('.').collect::<Vec<&str>>()[0];
+    let output_file_name = format!("{}{}", root_file_name, ".asm");
+    let mut parser = Parser::new(input_file_name);
+    let res = parser.parse();
+    let mut writer = CodeWriter::new(&output_file_name);
+    writer.write(res);
+}
---
+++ project7/commandtype.rs
@@ -0,0 +1,12 @@
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub enum CommandType {
+    C_ARITHMETIC,
+    C_PUSH,
+    C_POP,
+    C_LABEL,
+    C_GOTO,
+    C_IF,
+    C_FUNCTION,
+    C_RETURN,
+    C_CALL,
+}
